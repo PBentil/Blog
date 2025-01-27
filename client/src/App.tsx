5b5dbd52d824ee0b3c89437ec659bd9a84c09db5
PBentil <paapabentil122@gmail.com>
2025-01-27 08:52:23 +0000

    firsr commit

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//pages 
+import Home from './pages/Home'
+import About from './pages/About';
+import Article from './pages/Article';
+import ArticlesList from './pages/ArticlesList';
+//components
+import Navbar from './components/NavBar';
+
+function App() {
+
+
+  return (
+      <Router>
+        <Navbar />
+        <div className='max-w-screen-md mx-auto pt-20'>
+
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/About' element={<About />} />
+            <Route path='/ArticlesList' element={<ArticlesList />} />
+            <Route path='/Article/:name' element={<Article />} />
+          </Routes>
+        </div>
+      </Router>
+  )
+}
+
+export default App 
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+
+
+const Home = () => {
+    return (
+        <div className="mb-20">
+            <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">Hello, welcome to my Blog</h1>
+            <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam doloremque dolores animi, quia, magni eius excepturi, ipsum voluptates at iste assumenda placeat aspernatur cum obcaecati ad? Eos nobis quia earum.</p>
+            <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam doloremque dolores animi, quia, magni eius excepturi, ipsum voluptates at iste assumenda placeat aspernatur cum obcaecati ad? Eos nobis quia earum.</p>
+            <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam doloremque dolores animi, quia, magni eius excepturi, ipsum voluptates at iste assumenda placeat aspernatur cum obcaecati ad? Eos nobis quia earum.</p>
+        </div>
+    );
+}
+
+export default Home;
